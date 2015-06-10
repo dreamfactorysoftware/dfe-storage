3911baeb87bef68689d405d4f3561779572f8dc6
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-10 19:24:49 -0400

    Check for string configured name in mount point

--- src/Managers/MountManager.php
+++ src/Managers/MountManager.php
@@ -14,90 +14,80 @@ class MountManager extends BaseManager implements StorageMounter
     //******************************************************************************
 
     /**
-     * Mount the filesystem "$name" as defined in "config/filesystems.php"
+     * Mount the filesystem "$name" as defined in "config/flysystem.php"
      *
      * @param string $name
-     * @param array  $options
+     * @param array $options
      *
      * @return Filesystem
      * @throws \DreamFactory\Enterprise\Storage\Exceptions\MountException
      */
-    public function mount( $name, $options = [] )
+    public function mount($name, $options = [])
     {
-        $_tag = str_replace( '.', '-', IfSet::get( $options, 'tag', $name ) );
+        $_tag = str_replace('.', '-', IfSet::get($options, 'tag', $name));
 
-        if ( null !== ( $_prefix = IfSet::get( $options, 'prefix' ) ) )
-        {
-            $_prefix = rtrim( $_prefix ) . DIRECTORY_SEPARATOR;
+        if (null !== ($_prefix = IfSet::get($options, 'prefix'))) {
+            $_prefix = rtrim($_prefix) . DIRECTORY_SEPARATOR;
         }
 
-        try
-        {
-            return $this->resolve( $_tag );
-        }
-        catch ( \InvalidArgumentException $_ex )
-        {
+        try {
+            return $this->resolve($_tag);
+        } catch (\InvalidArgumentException $_ex) {
         }
 
         //  See if we have a pre-defined connection
-        if ( null === ( $_config = config( 'flysystem.connections.' . $_tag ) ) )
-        {
-            if ( empty( $options ) )
-            {
-                throw new MountException( 'No configuration found or specified for mount "' . $name . '".' );
+        if (null === ($_config = config('flysystem.connections.' . $_tag))) {
+            if (empty($options)) {
+                throw new MountException('No configuration found or specified for mount "' . $name . '".');
             }
 
             $_config = [];
         }
 
         //  Check for "path" or "root" in config...
-        if ( null === ( $_path = IfSet::get( $_config, 'path' ) ) && null === ( $_path = IfSet::get( $_config, 'root' ) ) )
-        {
-            throw new \InvalidArgumentException( 'No "path" or "root" defined for mount "' . $name . '"' );
+        if (null === ($_path = IfSet::get($_config, 'path')) && null === ($_path = IfSet::get($_config, 'root'))) {
+            throw new \InvalidArgumentException('No "path" or "root" defined for mount "' . $name . '"');
         }
 
-        if ( isset( $_config['root'] ) )
-        {
-            unset( $_config['root'] );
+        if (isset($_config['root'])) {
+            unset($_config['root']);
         }
 
         $_config['path'] = $_path;
 
-        !isset( $_config['driver'] ) && $_config['driver'] = 'local';
+        !isset($_config['driver']) && $_config['driver'] = 'local';
 
-        if ( !empty( $_prefix ) )
-        {
-            $_path = rtrim( $_config['path'], DIRECTORY_SEPARATOR );
-            $_prefix = trim( $_prefix, ' ' . DIRECTORY_SEPARATOR );
+        if (!empty($_prefix)) {
+            $_path = rtrim($_config['path'], DIRECTORY_SEPARATOR);
+            $_prefix = trim($_prefix, ' ' . DIRECTORY_SEPARATOR);
 
-            if ( false === strpos( $_path, $_prefix ) )
-            {
+            if (false === strpos($_path, $_prefix)) {
                 $_config['path'] = $_path . DIRECTORY_SEPARATOR . $_prefix;
             }
         }
 
-        config( ['flysystem.connections.' . $_tag => array_merge( $_config, $options )] );
+        config(['flysystem.connections.' . $_tag => array_merge($_config, $options)]);
 
         /** @noinspection PhpUndefinedMethodInspection */
         $this->manage(
             $_tag,
-            $_filesystem = Flysystem::connection( $_tag )
+            $_filesystem = Flysystem::connection($_tag)
         );
 
         return $_filesystem;
     }
 
     /**
-     * Unmount the filesystem "$name" as defined in "config/filesystems.php"
+     * Unmount the filesystem "$name" as defined in "config/flysystem.php"
      *
      * @param string $name
-     * @param array  $options
+     * @param array $options
      *
      * @return StorageMounter
      */
-    public function unmount( $name, $options = [] )
+    public function unmount($name, $options = [])
     {
-        return $this->unmanage( $name );
+        return $this->unmanage($name);
     }
 
 }
