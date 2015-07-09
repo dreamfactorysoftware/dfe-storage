b4e0e4c438db4683a9ab23d26e7677c61a075626
Jerry Ablan <jerryablan@dreamfactory.com>
2015-07-09 17:55:32 -0400

    Current work checkpoint. See diff for details.

--- src/Adapters/ZipArchiveAdapter.php
+++ src/Adapters/ZipArchiveAdapter.php
@@ -1,34 +1,13 @@
 <?php
 
-use League\Flysystem\Adapter\Local;
-use League\Flysystem\Filesystem;
+use DreamFactory\Enterprise\Common\Traits\Archivist;
 use League\Flysystem\ZipArchive\ZipArchiveAdapter as BaseZipArchiveAdapter;
 
 class ZipArchiveAdapter extends BaseZipArchiveAdapter
 {
     //******************************************************************************
-    //* Methods
+    //* Traits
     //******************************************************************************
 
-    /**
-     * @param string $tag      Unique identifier for temp space
-     * @param bool   $pathOnly If true, only the path is returned.
-     *
-     * @return \League\Flysystem\Filesystem|string
-     */
-    protected static function getWorkPath($tag, $pathOnly = false)
-    {
-        $_root = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'dfe' . DIRECTORY_SEPARATOR . $tag;
-
-        if (!\DreamFactory\Library\Utility\FileSystem::ensurePath($_root)) {
-            throw new \RuntimeException('Unable to create working directory "' . $_root . '". Aborting.');
-        }
-
-        if ($pathOnly) {
-            return $_root;
-        }
-
-        //  Set our temp base
-        return new Filesystem(new Local($_root));
-    }
+    use Archivist;
 }
